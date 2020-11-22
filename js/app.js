cc2eaa4c5910e9dd5235a9579d58b4a140cc3dc4
AmalIrfan <amalirfankc7@gmail.com>
2020-11-23 00:59:40 +0530

    .

--- js/app.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".dot").forEach((dot) => {
-    dot.addEventListener("click", (e) => {
-        document.body.className = e.target.id;
-        console.log(e);
-    });
-});
